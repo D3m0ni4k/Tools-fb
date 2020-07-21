2ee1421a78b59789bdef4e1c1c8d2154955b3113
D3m0ni4k <38411671+D3m0ni4k@users.noreply.github.com>
2020-07-21 17:17:33 +0200

    Add files via upload

---
+++ Tools.py
@@ -0,0 +1,66 @@
+#Coded By Zidan-ID\King Mr_Z17 & D3m0ni4k 
+
+import os,sys,time
+
+
+def main():
+    time.sleep(1)
+    os.system ('clear')
+    print '[96m'
+    os.system ('figlet D3m0ni4k')
+    print'[92m================================='
+    print'[97m Author  : [96mD3m0ni4k '
+    print'[97m Team    : [96mR3v0lt '
+    print'[97m Credits : [96mCoded By Zidan-ID\King Mr_Z17 & D3m0ni4k '
+    print'[92m================================='
+    print'[92m++++++++++++ [97mTOOLS [92m++++++++++++'
+    print'[92m[[97m1[92m] [97mDark Fb v1.7 '
+    print'[92m[[97m2][92m [97mDark Fb v1.8 '
+    print'[92m[[97m3[92m] [97mDark Fb Premium '
+    print'[92m[[97m4[92m] [97mDark Fb New'
+    print'[92m[[97m5[92m] [97mDark Fb Diamond '
+    print'[92m[[91m0[92m] [91mExit '
+    gans = raw_input ('[97m==>[93m ')
+    if gans in ['1']:
+        time.sleep(1)
+        os.system ('git clone https://github.com/Mr-XsZ/Dark-Fb')
+        print ('[97m$ lanjut ketikan [93mcd Dark-Fb')
+        print ('[97m$ lalu [93mpython2 dark.py')
+        exit()
+    if gans in ['2']:
+        time.sleep(1)
+        os.system ('git clone https://github.com/wira2611/Pro2611')
+        print ('[97m$ lanjut ketikan [93mcd Pro2611')
+        print ('[97m$ lalu[93m python2 Pro.py')
+        exit()
+    if gans in ['3']:
+        time.sleep(1)
+        os.system ('git clone https://github.com/TheMagizz/DarkPremium')
+        print ('[97m$lanjut ketikan [93mcd DarkPremium')
+        print ('[97m$ lalu [93mpython2 DarkFB.py')
+        exit()
+    if gans in ['4']:
+        time.sleep(1)
+        os.system ('git clone https://github.com/KingMrZ17/DarkFbNew')
+        print ('[97m$ lanjut ketikan [93mcd DarkFbNew')
+        print ('[97m$ lalu [93mpython2 darkfb.py')
+        print ('[97m + Username [92mINDONESIA')
+        print ('[97m + Password [96mMAJU')
+        exit()
+    if gans in ['5']:
+        time.sleep(1)
+        os.system('git clone https://github.com/wira2611/diamon')
+        print ('[97m$ lanjut ketikan[93m cd diamon')
+        print ('[97m$ lalu [93m python2 diamond-1.py')
+        exit()
+    if gans in ['0']:
+        time.sleep(2)
+        exit()
+    else:
+        time.sleep(1)
+        print '[97m Pilih Yang Bener [92mGoblok . . .'
+        time.sleep(1)
+        main()
+
+main()
+ 
